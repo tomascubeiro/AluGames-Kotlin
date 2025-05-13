1683aef7fada8f6f8182e71b48f8af267faecc45
Tomás Cubeiro <tocubeiro@gmail.com>
2025-05-13 15:30:53 -0300

    Adicionando data class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,7 +17,11 @@ fun main() {
     println(json)
 
     val gson = Gson()
-    val meuJogo = gson.fromJson(json, InfoJogo::class.java)
+    val meuInfoJogo = gson.fromJson(json, InfoJogo::class.java)
+
+    val meuJogo = Jogo(
+        meuInfoJogo.info.title,
+        meuInfoJogo.info.thumb)
 
     println(meuJogo)
 }
