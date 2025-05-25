a3cf094e433d1091283a5467a58b78baeba6b026
Tomas Cubeiro <tocubeiro@gmail.com>
2025-05-25 20:09:49 -0300

    Adicionando extension functions e finalizando etapa 1.

--- src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -25,12 +25,12 @@ data class Gamer(var nome:String, var email:String){
             }
 
 
-    init{
-        if (nome.isNullOrBlank()){
-            throw IllegalArgumentException("Nome está em branco.")
-        }
-        this.email = validarEmail()
-    }
+//    init{
+//        if (nome.isNullOrBlank()){
+//            throw IllegalArgumentException("Nome está em branco.")
+//        }
+//        this.email = validarEmail()
+//    }
 
     override fun toString(): String {
         return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)"
--- src/main/kotlin/br/com/alura/alugames/principal/Main.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/Main.kt
@@ -3,6 +3,7 @@ package br.com.alura.alugames.principal
 import br.com.alura.alugames.modelo.Gamer
 import br.com.alura.alugames.modelo.Jogo
 import br.com.alura.alugames.servicos.ConsumoApi
+import transformarEmIdade
 import java.util.Scanner
 
 
@@ -12,6 +13,9 @@ fun main() {
     val gamer = Gamer.criarGamer(leitura)
     println("Cadastro concluído com sucesso. Dados do gamer:")
     println(gamer)
+    println("Idade do gamer: " + gamer.dataNascimento?.transformarEmIdade())
+
+
 
     do {
         println("Digite um código de jogo para buscar:")
@@ -55,6 +59,35 @@ fun main() {
 
     println("Jogos buscados:")
     println(gamer.jogosBuscados)
+
+    println("\n Jogos ordenados por título: ")
+    gamer.jogosBuscados.sortBy {
+        it?.titulo
+    }
+
+    gamer.jogosBuscados.forEach {
+        println("Título: " + it?.titulo)
+    }
+
+    val jogosFiltrados = gamer.jogosBuscados.filter {
+        it?.titulo?.contains("batman", ignoreCase = true) ?: false
+    }
+    println("Jogos filtrados: ")
+    println(jogosFiltrados)
+
+    println("Deseja excluir algum jogo da lista original? S/N")
+    val opcao = leitura.nextLine()
+    if(opcao.equals("s", ignoreCase = true)){
+        println(gamer.jogosBuscados)
+        println("\n Informe a posição do jogo que deseja excluir: ")
+
+        val posicao = leitura.nextInt()
+        gamer.jogosBuscados.removeAt(posicao)
+    }
+
+    println("\n Lista atualizada: ")
+    println(gamer.jogosBuscados)
+
     println("Busca finalizada com sucesso.")
 
 }
